7a320823742853cf13a191b89ba6bf4b42a3e449
Nikita <76918459+Urodec777@users.noreply.github.com>
2022-05-11 02:40:11 +0300

    Update views.py

--- fizikl/definedate/views.py
+++ fizikl/definedate/views.py
@@ -6,8 +6,11 @@ from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 
 """ rendering index page with form """
+
+
 class Index(TemplateView):
     template_name = 'index.html'
+
 
 class DateView(GenericAPIView):
 
@@ -16,8 +19,10 @@ class DateView(GenericAPIView):
             serializer = DateSerializer(data={'date': request.GET['date']})
         except:
             # this line of code to prevent 500 error when user comes to /ajax/ in DRF
-            serializer = DateSerializer(data={'date': datetime.datetime.now().strftime('%d / %m / %Y')})
+            serializer = DateSerializer(
+                data={'date': datetime.datetime.now().strftime('%d / %m / %Y')})
         # getting datetime instance of selected date to check year
-        date = datetime.datetime.strptime(serializer.initial_data.get('date'), '%d / %m / %Y')
+        date = datetime.datetime.strptime(
+            serializer.initial_data.get('date'), '%d / %m / %Y')
         if serializer.is_valid(raise_exception=True) and date.year >= 2019:
-            return Response(data = serializer.validated_data.get('date').strftime('%U'))
+            return Response(data=serializer.validated_data.get('date').strftime('%U'))
